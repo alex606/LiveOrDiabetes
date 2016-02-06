d7efd5daceaf9fca8f09c528a0aecc76b746d6b6
cheese42 <cheese42@ksu.edu>
2016-02-06 12:40:37 -0600

    Melee attack code from youtube tutorial https://www.youtube.com/watch?v=xwPahXLpNh8

---
+++ GameJamProjectFolder/Assets/scripts/AttackTrigger.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class attackTrigger : MonoBehavior 
+{
+	
+	public int dmg = 20;
+	
+	void OnTriggerEnter20(Collider2D col)
+	{
+		if (col.isTrigger is true && col.CompareTag("Enemy"))
+		{
+			col.SendMessageUpwards("Damage", dmg)
+		}
+	}
+}
