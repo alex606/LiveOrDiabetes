c4dce13fd1b4f2727bc4de798b807ce424cff190
Alexander Wu <alex606@att.net>
2016-02-07 04:53:04 -0600

    Added StartMenu

---
+++ GameJamProjectFolder/Assets/scripts/StartScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour
+{
+    public string FirstLevel;
+
+    void Update ()
+    {
+        if(!Input.GetMouseButtonDown(0))
+        {
+            return;
+        }
+        Application.LoadLevel(FirstLevel);
+	}
+}
