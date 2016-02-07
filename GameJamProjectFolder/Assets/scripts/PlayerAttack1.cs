7d7b80bede3add645a5f846e976d2510c50ece43
Serialbocks <johnj.gw@gmail.com>
2016-02-06 21:11:17 -0600

    level stuff...and stuff.

---
+++ GameJamProjectFolder/Assets/scripts/PlayerAttack1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack1 : MonoBehaviour
+{
+    private bool _attacking = false;
+
+    private float _attackTimer = 0;
+    private float _attackCooldown = 0.3f;
+
+    public Collider2D attackTrigger;
+
+    private Animator _animator;
+
+    public void Awake()
+    {
+        //_animator = gameObject.GetComponent<Animator>();
+        attackTrigger.enabled = false;
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown("z") && !_attacking)
+        {
+            _attacking = true;
+            _attackTimer = _attackCooldown;
+
+            attackTrigger.enabled = true;
+        }
+
+        if(_attacking)
+        {
+            if(_attackTimer > 0)
+            {
+                _attackTimer -= Time.deltaTime;
+            }
+            else
+            {
+                _attacking = false;
+                attackTrigger.enabled = false;
+            }
+        }
+    }
+}
