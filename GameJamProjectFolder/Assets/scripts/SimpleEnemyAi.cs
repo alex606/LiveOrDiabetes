117f9d698d21ad71a0b2996a37c15fed5dcd82eb
cheese42 <cheese42@ksu.edu>
2016-02-06 23:48:07 -0600

    things stop jumping off ledges

--- GameJamProjectFolder/Assets/scripts/SimpleEnemyAi.cs
+++ GameJamProjectFolder/Assets/scripts/SimpleEnemyAi.cs
@@ -7,10 +7,11 @@ public class SimpleEnemyAi : MonoBehaviour, ITakeDamage
     public float Speed;
     public float FireRate = 1;
 
+    private bool grounded;
     private CharacterController2D _controller;
     private Vector2 _direction;
     private Vector2 _startPosition;
-    
+
 
     public void Start()
     {
@@ -19,11 +20,24 @@ public class SimpleEnemyAi : MonoBehaviour, ITakeDamage
         _startPosition = transform.position;
     }
 
+    public void FixedUpdate()
+    {
+
+        grounded = GetComponent<CharacterController2D>().State.IsGrounded;
+
+        if (!grounded)
+        {
+            _direction = -_direction;
+        }
+
+    }
+
     public void Update()
     {
         _controller.SetHorizontalForce(_direction.x * Speed);
 
-        if((_direction.x < 0 && _controller.State.IsCollidingLeft) || (_direction.x > 0 && _controller.State.IsCollidingRight))
+
+        if ((_direction.x < 0 && _controller.State.IsCollidingLeft) || (_direction.x > 0 && _controller.State.IsCollidingRight))
         {
             _direction = -_direction;
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
