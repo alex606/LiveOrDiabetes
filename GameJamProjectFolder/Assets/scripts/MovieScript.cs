14624cc86013beceaf80c0bf2e555727f7bcd1c9
cheese42 <cheese42@ksu.edu>
2016-02-07 11:45:28 -0600

    46 files?!
    also Wu's stuff

---
+++ GameJamProjectFolder/Assets/scripts/MovieScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieScript : MonoBehaviour
+{
+
+    public MovieTexture MovieTexture;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.mainTexture = MovieTexture;
+    }
+}
