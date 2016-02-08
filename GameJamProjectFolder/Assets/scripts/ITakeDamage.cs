cef17c67406140bdaf066d000c802d86c1995818
Darkinfurno <svengerman@gmail.com>
2016-02-07 22:32:30 -0600

    Implemented Projectiles and damage.

---
+++ GameJamProjectFolder/Assets/scripts/ITakeDamage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+
+    public interface ITakeDamage
+    {
+        void TakeDamage(int damage, GameObject instigator);
+
+    }
+
--- GameJamProjectFolder/Assets/scripts/Player.cs
+++ GameJamProjectFolder/Assets/scripts/Player.cs
@@ -13,12 +13,23 @@ public class Player : MonoBehaviour
     public float SpeedAccelerationInAir = 5f;
     public int MaxHealth = 100;
     public GameObject HealthBar;
+
+    public Projectile Projectile;
+    public float FireRate;
+    public Transform ProjectileFireLocation;
+
+    public AudioClip PlayerHitSound;
+    public AudioClip PlayerMeleeSound;
+    public AudioClip PlayerShootSound;
+    public AudioClip PlayerDeathSound;
+	
     // public GameObject OuchEffect;
 
     public int Health { get; private set; }
     public bool IsDead { get; private set; }
 
     public Animator Animator;
+    private float _canFireIn;
 
     public void Awake()
     {
@@ -31,6 +42,8 @@ public class Player : MonoBehaviour
 
     public void Update()
     {
+        _canFireIn -= Time.deltaTime;
+
         if (!IsDead) HandleInput();
 
         var movementFactor = _controller.State.IsGrounded ? SpeedAccelerationOnGround : SpeedAccelerationInAir;
@@ -50,6 +63,7 @@ public class Player : MonoBehaviour
 
     public void Kill()
     {
+        AudioSource.PlayClipAtPoint(PlayerDeathSound, transform.position);
         _controller.HandleCollisions = false;
         GetComponent<Collider2D>().enabled = false;
         IsDead = true;
@@ -74,6 +88,7 @@ public class Player : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
+        AudioSource.PlayClipAtPoint(PlayerHitSound, transform.position);
         // Instantiate(OuchEffect, transform.position, transform.rotation);
         Health -= damage;
         _healthController.UpdateHealth(Health, MaxHealth);
@@ -109,6 +124,23 @@ public class Player : MonoBehaviour
         {
             _controller.Jump();
         }
+
+        if (Input.GetKeyDown(KeyCode.X))
+            FireProjectile();
+    }
+
+    private void FireProjectile()
+    {
+        if (_canFireIn > 0) return;
+
+        var direction = _isFacingRight ? Vector2.right : Vector2.left;
+
+        var projectile = (Projectile)Instantiate(Projectile, ProjectileFireLocation.position, ProjectileFireLocation.rotation);
+        Projectile.Initialize(gameObject, direction, _controller.Velocity);
+
+        _canFireIn = FireRate;
+
+        AudioSource.PlayClipAtPoint(PlayerShootSound, transform.position);
     }
 
     private void Flip()
